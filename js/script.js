caeb9e4cbab3edfa13fca00415ef9b782393a794
tobechukwu <tobeblast06@gmail.com>
2025-07-31 14:54:28 +0200

    Completed everything ���

--- js/script.js
+++ js/script.js
@@ -1,9 +1,19 @@
 // Our Team Section Under about us page
 const imageEl = document.querySelectorAll("#img");
 const socialLinksEl = document.querySelectorAll(".social-links-div");
+const categoryEl = document.querySelectorAll(".work-category");
 
+// About Us Section
 imageEl.forEach((img, index) => {
   img.addEventListener("click", () => {
     socialLinksEl[index].classList.toggle("hidden");
+  });
+});
+
+// Work Section
+categoryEl.forEach((category) => {
+  category.addEventListener("click", () => {
+    categoryEl.forEach((p) => p.classList.remove("clicked"));
+    category.classList.add("clicked");
   });
 });
